7baf80de8e0531ea7d390a8246342c948237d1f3
xechezurieta <xabier.echezurieta@gmail.com>
2021-10-06 21:18:04 +0200

    add porPais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,39 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country, Error } from '../../intefaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  termino: string = 'Hola Mundo';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private pservice: PaisService) {}
+
+  buscar() {
+    console.log(this.termino);
+    this.hayError = false;
+    this.pservice.buscarPais(this.termino).subscribe(
+      (resp) => {
+        if (resp.status) {
+          this.hayError = true;
+          this.paises = [];
+        } else {
+          this.paises = resp;
+        }
+
+        console.log('Paises' + JSON.stringify(this.paises));
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
+  }
+}
